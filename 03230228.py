558151678ba08db3cd6ad10d228a1c0cd898071d
my_GitHub <03230228.gcbs@rub.edu.bt>
2024-05-26 00:45:31 +0600

    Included all

--- 03230228.py
+++ 03230228.py
@@ -1,3 +1,14 @@
+# https://github.com/Pepdep2023/03230228_BIA101B_CAP3
+# Phuntsho Dema
+# BBI 'B'
+# 03230228
+########################################################################
+# References:
+# https://medium.com/@promoterseo.com/python-read-text-file-in-visual-studio-code-how-to-read-a-text-file-e672fbd2024c
+# https://www.google.com/gasearch?q=how%20to%20read%20txt%20file%20in%20python%20using%20vscide&tbm=&source=sh/x/gs/m2/5#fpstate=ive&vld=cid:20840800,vid:XPO6ITK2Ecc,st:0
+# https://stackoverflow.com/questions/55191397/vscode-read-file-from-current-folder-where-py-file-is
+########################################################################
+# Solution
 filename = "228.txt"
 
 with open(filename, "r") as f:
@@ -14,7 +25,13 @@ with open(filename, "r") as f:
     # Combine and add to total
     total_sum += first_digit * 10 + last_digit
 
-print(f"Sum of combined first and last digits: {total_sum}")
+print(f"Sum of first digit and last digit from each 10,000 lines: {total_sum}")
+
+# Solution I got in my terminal
+# Sum of first digit and last digit from each 10,000 lines: 217780
+
+
+
 
 
 
