da23256b66538f6c462e4ac75ae54954cbfd0098
my_GitHub <03230228.gcbs@rub.edu.bt>
2024-05-26 00:47:24 +0600

    final

--- 03230228.py
+++ 03230228.py
@@ -30,6 +30,12 @@ print(f"Sum of first digit and last digit from each 10,000 lines: {total_sum}")
 # Solution I got in my terminal
 # Sum of first digit and last digit from each 10,000 lines: 217780
 
+# Gettings from the code
+# 1. file = "228.txt" :This line opens the file specified by filename in read mode ("r") and assigns the file object to the variable f. The with statement ensures that the file is automatically closed after the indented block of code is executed, even if an exception occurs.
+# 2. 1. Empty Line Handling: The code now includes an if statement to check if the line is empty ( not line ). If it is, it skips to the next line using continue . This ensures non-empty lines are processed.
+# 3. Handling Non-Digit Characters: The logic for extracting the first and last digits remains the same. However, if the first or last character is not a digit, it's converted to 0 using a ternary operator (int(line[0]) if
+#line[0]. isdigit() else 0 ). This ensures only digits are considered for the sum.(continuation)
+# 4. Combining and Adding: The extracted digits are combined into a single number by multiplying the first digit by 10 (to place it in the tens place) and adding the last digit. This combined number is then added to the total _sum variable.
 
 
 
